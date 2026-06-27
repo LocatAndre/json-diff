89cdacc32bf7d17a49bc390d7bf1cbd36b881132
Andrea Locatelli <ciao.loca@gmail.com>
2026-06-27 12:09:22 +0200

    feat: github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/json-diff",
   plugins: [
     react(),
     tailwindcss(),
