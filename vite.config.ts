79cf46c1d3c58d576451155c1ed17d8172276123
Andrea Locatelli <ciao.loca@gmail.com>
2026-06-27 12:19:38 +0200

    feat: fix for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/json-diff",
+  base: "/json-diff/",
   plugins: [
     react(),
     tailwindcss(),
